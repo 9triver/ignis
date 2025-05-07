51775cddc85a3469405ab66801f5a67f333322b8
Tianqi Ren <tianqi.r@outlook.com>
2025-05-07 14:06:36 +0800

    chore: reorganize proto messages

--- ignis-proto/protobuf-gen.sh
+++ ignis-proto/protobuf-gen.sh
@@ -6,24 +6,34 @@ export PATH="$PATH:$HOME/go/bin"
 ACTOR_SRC=$(go list -f {{.Dir}} github.com/asynkron/protoactor-go/actor)
 ACTOR_PROTO=$ACTOR_SRC/actor.proto
 
-PROTOC="$PROTOC -I $ACTOR_SRC -I ./messages"
-PROTO_SRC="./messages/*.proto ./messages/executor/*.proto ./messages/controller/*.proto ./messages/cluster/*.proto"
+PROTOC="$PROTOC -I $ACTOR_SRC -I ."
+PROTO_SRC="*.proto ./executor/*.proto ./controller/*.proto ./cluster/*.proto"
 
-if [ ! -d ./python ]; then
-  echo "Creating output directory for Python: ./python"
-  mkdir -p ./python
-fi
+GO_OUTPUTS="../ignis-go/proto"
+PY_OUTPUTS="../clients/py/actorc/protos"
 
-echo "Generating protobuf files for Go"
-$PROTOC --go_out=../ignis-go/proto --go_opt=paths=source_relative --go-grpc_out=../ignis-go/proto --go-grpc_opt=paths=source_relative $PROTO_SRC
+for GO_OUTPUT in $GO_OUTPUTS; do
+  echo "Generating protobuf files for Go: $GO_OUTPUT"
 
-PY_OUTPUTS="./python ../clients/py/actorc/protos"
-
-echo "Generating protobuf files for Python"
-for PY_OUTPUT in $PY_OUTPUTS; do
-  if [ ! -d $PY_OUTPUT ]; then
-    echo "Creating output directory for Python: $PY_OUTPUT"
-    mkdir -p $PY_OUTPUT
+  if [ ! -d $GO_OUTPUT ]; then
+    mkdir -p $GO_OUTPUT
+  else
+    find $GO_OUTPUT -type f -name "*.pb.go" -delete
   fi
+
+  $PROTOC --go_out=$GO_OUTPUT --go_opt=paths=source_relative --go-grpc_out=$GO_OUTPUT --go-grpc_opt=paths=source_relative $PROTO_SRC
+done
+
+for PY_OUTPUT in $PY_OUTPUTS; do
+  echo "Generating protobuf files for Python: $PY_OUTPUT"
+
+  if [ ! -d $PY_OUTPUT ]; then
+    mkdir -p $PY_OUTPUT
+  else
+    find $PY_OUTPUT -type f -name "*_pb2.py" -delete
+    find $PY_OUTPUT -type f -name "*_pb2.pyi" -delete
+    find $PY_OUTPUT -type f -name "*_pb2_grpc.py" -delete
+  fi
+
   $PROTOC --python_out=$PY_OUTPUT --pyi_out=$PY_OUTPUT --grpc_python_out=$PY_OUTPUT $PROTO_SRC $ACTOR_PROTO
 done
